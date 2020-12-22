2d6ebc3d42bde3f9f16b252e0181c4307a40c619
Craig Taylor <craig.taylor@sevenwhitepixels.co.uk>
2020-12-22 12:03:54 +0000

    additional select options on the control panel

--- src/components/ControlPanel/ControlPanel.js
+++ src/components/ControlPanel/ControlPanel.js
@@ -1,14 +1,32 @@
 import IconButton from '@material-ui/core/IconButton';
 import CropFreeIcon from '@material-ui/icons/CropFree';
+import GestureIcon from '@material-ui/icons/Gesture';
+import FormatShapesIcon from '@material-ui/icons/FormatShapes';
 
+import ControlPanelTools from './../../components/ControlPanel/ControlPanelTools';
 import './control-panel.scss';
+
+
 
 export default function ControlPanel(props) {
   
+  const selectTool = selection => {
+    console.log(selection);
+  }
+
   return (
     <div className='control-panel'>
-      <IconButton aria-label="bounding box" component="span">
+      <IconButton aria-label="bounding box" component="span"
+        onClick={() => { selectTool(ControlPanelTools.BOUNDING_BOX);}}>
         <CropFreeIcon />
+      </IconButton>
+      <IconButton aria-label="polygon" component="span"
+        onClick={() => { selectTool(ControlPanelTools.POLYGON);}}>
+        <FormatShapesIcon />
+      </IconButton>
+      <IconButton aria-label="free select" component="span"
+        onClick={() => { selectTool(ControlPanelTools.FREE_SELECT);}}>
+        <GestureIcon />
       </IconButton>
     </div>
   )
---
+++ src/components/ControlPanel/ControlPanelTools.js
@@ -0,0 +1,6 @@
+export default {
+  NONE: 'none',
+  BOUNDING_BOX: 'bounding-box',
+  POLYGON: 'polygon',
+  FREE_SELECT: 'free-select'
+};
